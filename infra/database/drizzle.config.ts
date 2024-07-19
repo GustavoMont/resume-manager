70add0b4f5f57d55afbc98ec1faf93defb2758f0
GustavoMont <lgmluisgm@gmail.com>
2024-07-18 23:27:30 -0300

    configs create migrations script

---
+++ infra/database/drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'drizzle-kit';
+import { resolve } from "node:path"
+
+export default defineConfig({
+  schema: resolve('.', 'infra', 'database', 'schema.ts'),
+  out: resolve('.', 'infra', 'database', 'migrations'),
+  dialect: 'postgresql', // 'postgresql' | 'mysql' | 'sqlite'
+  dbCredentials: {
+    url: process.env.DATABASE_URL
+  },
+});
