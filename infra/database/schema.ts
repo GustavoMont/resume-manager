e3491fced03f1e04c47cb9ef2c9108ad72825e75
GustavoMont <lgmluisgm@gmail.com>
2024-07-22 21:22:55 -0300

    update users schema

--- infra/database/schema.ts
+++ infra/database/schema.ts
@@ -1,3 +1,20 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {});
+const commonFields = {
+  createdAt: timestamp("created_at", {
+    mode: "string",
+    withTimezone: false,
+  }).defaultNow(),
+  updatedAt: timestamp("updated_at", {
+    mode: "string",
+    withTimezone: false,
+  }).defaultNow(),
+};
+
+export const users = pgTable("users", {
+  firstName: varchar("first_name", { length: 255 }),
+  lastName: varchar("last_name", { length: 255 }),
+  email: varchar("email", { length: 255 }).unique(),
+  password: varchar("password", { length: 255 }),
+  ...commonFields,
+});
