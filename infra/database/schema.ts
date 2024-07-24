686b2433f5d54359a11c892b69bc2892e53d197f
GustavoMont <lgmluisgm@gmail.com>
2024-07-23 22:00:24 -0300

    Revert "update users schema"
    This reverts commit e3491fced03f1e04c47cb9ef2c9108ad72825e75.

--- infra/database/schema.ts
+++ infra/database/schema.ts
@@ -1,20 +1,3 @@
-import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
 
-const commonFields = {
-  createdAt: timestamp("created_at", {
-    mode: "string",
-    withTimezone: false,
-  }).defaultNow(),
-  updatedAt: timestamp("updated_at", {
-    mode: "string",
-    withTimezone: false,
-  }).defaultNow(),
-};
-
-export const users = pgTable("users", {
-  firstName: varchar("first_name", { length: 255 }),
-  lastName: varchar("last_name", { length: 255 }),
-  email: varchar("email", { length: 255 }).unique(),
-  password: varchar("password", { length: 255 }),
-  ...commonFields,
-});
+export const users = pgTable("users", {});
