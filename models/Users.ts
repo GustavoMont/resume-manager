878a1640b5f21c109723f115a752338a7463f4b9
GustavoMont <lgmluisgm@gmail.com>
2024-07-25 08:06:06 -0300

    create `User` model

--- models/Users.ts
+++ models/Users.ts
@@ -1,9 +1,12 @@
+import { getTableColumns } from "drizzle-orm";
 import database from "infra/database/database";
 import { users } from "infra/database/schema";
 
 async function getAllUsers() {
   const db = await database.getNewDb();
-  const result = await db.select().from(users);
+  const columns = getTableColumns(users);
+  delete columns.password;
+  const result = await db.select(columns).from(users);
   return result;
 }
 
