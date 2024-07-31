ddf84edd2d740e7287431fd48485ed4506e3c6c1
GustavoMont <lgmluisgm@gmail.com>
2024-07-31 08:18:46 -0300

    feat(UsersModel): create `createUser` method

--- models/Users.ts
+++ models/Users.ts
@@ -1,6 +1,8 @@
-import { getTableColumns } from "drizzle-orm";
+import { eq, getTableColumns } from "drizzle-orm";
 import database from "infra/database/database";
 import { users } from "infra/database/schema";
+import { User } from "types/User";
+import Password from "./Password";
 
 async function getAllUsers() {
   const db = await database.getNewDb();
@@ -10,6 +12,37 @@ async function getAllUsers() {
   return result;
 }
 
-const UserModel = { getAllUsers };
+async function hashUserPassword(user: User) {
+  user.password = await Password.hashPassword(user.password);
+
+  return user;
+}
+
+async function getUserByEmail(email: string) {
+  const db = await database.getNewDb();
+  const [user] = await db.select().from(users).where(eq(users.email, email));
+  return user;
+}
+
+async function isNewUser(email: string) {
+  const isUserSignedUp = await getUserByEmail(email);
+  if (isUserSignedUp) {
+    throw {
+      message: "Usuário já cadastrado",
+      status: 400,
+    };
+  }
+}
+
+async function createUser(newUser: User) {
+  await isNewUser(newUser.email);
+  await hashUserPassword(newUser);
+  const db = await database.getNewDb();
+  const [user] = await db.insert(users).values(newUser).returning();
+
+  return user;
+}
+
+const UserModel = { getAllUsers, createUser };
 
 export default UserModel;
