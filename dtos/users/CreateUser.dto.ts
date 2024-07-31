4e373046b7d6e30462ac79207a4d7a7da9a4cc2f
GustavoMont <lgmluisgm@gmail.com>
2024-07-31 08:17:45 -0300

    feat(dto): create `CreateUserDeto` using `class-validator` and `class-transform`

---
+++ dtos/users/CreateUser.dto.ts
@@ -0,0 +1,25 @@
+import { Exclude } from "class-transformer";
+import { IsEmail, IsStrongPassword, MinLength } from "class-validator";
+import { User } from "types/User";
+
+export default class CreateUserDto implements User {
+  @Exclude()
+  createdAt: string;
+  @Exclude()
+  updatedAt: string;
+  @MinLength(3, { message: "Primeiro nome deve ter no mínimo 3 letras" })
+  firstName: string;
+  @MinLength(3, { message: "Sobrenome deve ter no mínimo 3 letras" })
+  lastName: string;
+  @IsStrongPassword(
+    { minLength: 8, minNumbers: 1 },
+    {
+      message: "Senha deve conter no mínimo 8 caracteres, com números e letras",
+    },
+  )
+  password: string;
+  @IsEmail(undefined, {
+    message: "E-mail inválido",
+  })
+  email: string;
+}
