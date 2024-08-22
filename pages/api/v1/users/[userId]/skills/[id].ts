023168fbb4c0744b16803e9ce48892d7d92a5016
GustavoMont <lgmluisgm@gmail.com>
2024-08-22 07:57:08 -0300

    refactor: create `skills-controller`

--- pages/api/v1/users/[userId]/skills/[id].ts
+++ pages/api/v1/users/[userId]/skills/[id].ts
@@ -1,14 +1,9 @@
-import { plainToInstance } from "class-transformer";
-import { validateOrReject } from "class-validator";
 import routeConfig from "config/api/route-config";
-import CreateSkillDto from "dtos/skills/CreateSkill.dto";
+import skillController from "controller/skill-controller";
 import authorGuard from "guards/author-guard";
-import { camelizeKeys, decamelizeKeys } from "humps";
 import Authentication from "models/Authentication";
-import Skills from "models/Skills";
 import { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
-import exceptionHandler from "utils/exceptions-handler";
 
 export const config = routeConfig.DEFAULT_ROUT_CONFIG;
 
@@ -17,56 +12,10 @@ const router = createRouter<NextApiRequest, NextApiResponse>();
 const jwtStrategy = Authentication.getJwtStrategy();
 const userIdParamGuard = authorGuard.userIdParamGuard;
 
-async function get(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { userId: userIdParam, id: idParam } = req.query;
-    const userId = Number(userIdParam);
-    const id = Number(idParam);
-
-    const skill = await Skills.getSkill({ userId, id });
-
-    return res.status(200).json(skill);
-  } catch (error) {
-    const errorMessage = exceptionHandler.handleException(error);
-    const statusCode = exceptionHandler.handleStatusCode(error);
-    return res.status(statusCode).json(errorMessage);
-  }
-}
-
-async function put(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const body = camelizeKeys(req.body);
-    const payload = plainToInstance(CreateSkillDto, body);
-    await validateOrReject(payload);
-    const { userId: userIdParam, id: idParam } = req.query;
-    const userId = Number(userIdParam);
-    const id = Number(idParam);
-    const updatedSkill = await Skills.updateSkill({ id, userId }, payload);
-
-    return res.status(200).json(decamelizeKeys(updatedSkill));
-  } catch (error) {
-    const statusCode = exceptionHandler.handleStatusCode(error);
-    const errorResponse = exceptionHandler.handleException(error);
-    return res.status(statusCode).json(errorResponse);
-  }
-}
-
-async function deleteSkill(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { id: idParam, userId: userIdParam } = req.query;
-    const userId = +userIdParam;
-    const id = +idParam;
-
-    await Skills.deleteSkill({ id, userId });
-
-    return res.status(204).end();
-  } catch (error) {
-    const statusCode = exceptionHandler.handleStatusCode(error);
-    const errorResponse = exceptionHandler.handleException(error);
-    return res.status(statusCode).json(errorResponse);
-  }
-}
-
-router.use(jwtStrategy, userIdParamGuard).get(get).put(put).delete(deleteSkill);
+router
+  .use(jwtStrategy, userIdParamGuard)
+  .get(skillController.get)
+  .put(skillController.put)
+  .delete(skillController.deleteSkill);
 
 export default router.handler();
--- pages/api/v1/users/[userId]/skills/index.ts
+++ pages/api/v1/users/[userId]/skills/index.ts
@@ -1,14 +1,9 @@
-import { plainToInstance } from "class-transformer";
-import { validateOrReject } from "class-validator";
 import routeConfig from "config/api/route-config";
-import CreateSkillDto from "dtos/skills/CreateSkill.dto";
+import skillController from "controller/skill-controller";
 import authorGuard from "guards/author-guard";
-import { decamelizeKeys } from "humps";
 import Authentication from "models/Authentication";
-import Skills from "models/Skills";
 import { createRouter } from "next-connect";
 import { NextApiRequest, NextApiResponse } from "next/types";
-import exceptionHandler from "utils/exceptions-handler";
 
 export const config = routeConfig.DEFAULT_ROUT_CONFIG;
 
@@ -17,29 +12,8 @@ const router = createRouter<NextApiRequest, NextApiResponse>();
 const jwtGuard = Authentication.getJwtStrategy();
 const userIdGuard = authorGuard.userIdParamGuard;
 
-async function getSkills(req: NextApiRequest, res: NextApiResponse) {
-  const skills = await Skills.getUserSkills(req.user.id);
-
-  return res.status(200).json(skills);
-}
-
-async function createSkill(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const payload = plainToInstance(CreateSkillDto, req.body);
-    await validateOrReject(payload);
-    const skill = await Skills.createSkill(req.user.id, payload);
-
-    return res.status(201).json(decamelizeKeys(skill));
-  } catch (error) {
-    const statusCode = exceptionHandler.handleStatusCode(error);
-    const errorResponse = exceptionHandler.handleException(error);
-
-    return res.status(statusCode).json(errorResponse);
-  }
-}
-
 export default router
   .use(jwtGuard, userIdGuard)
-  .get(getSkills)
-  .post(createSkill)
+  .get(skillController.getSkillsList)
+  .post(skillController.post)
   .handler();
