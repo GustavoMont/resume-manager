36dfc5f286e3e7fd24605447636adfebf7c870ff
GustavoMont <lgmluisgm@gmail.com>
2025-11-20 14:45:29 -0300

    :tada: start project

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from "react";
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      É um site que não sei se vai pra fente.
+      <p>
+        quero só testar ter mais de um banco: um pra aplicação e outro pros
+        testes
+      </p>
+    </div>
+  );
+}
