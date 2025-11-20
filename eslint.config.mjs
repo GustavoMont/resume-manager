440d9b77011893e8335c9d8eb4bb37e0d28202f0
GustavoMont <lgmluisgm@gmail.com>
2025-11-20 15:29:17 -0300

    style: config `prettier`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
 const eslintConfig = defineConfig(
   [
     ...nextVitals,
@@ -16,7 +18,8 @@ const eslintConfig = defineConfig(
     ]),
   ],
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 );
 
 export default eslintConfig;
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  endOfLine: "lf",
+  semi: true,
+};
+
+export default config;
